b783521e4fe9d4741eba2e51ad472cd0c52802bc
mergez <ndjemhi@gmail.com>
2021-12-06 00:43:18 -0500

    Fix Delete

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,7 +28,7 @@ $("#update_user").submit(function(event){
 
 })
 
-if(window.location.pathname == "/"){
+if(window.location.pathname == "/users"){
     $ondelete = $(".table tbody td a.delete");
     $ondelete.click(function(){
         var id = $(this).attr("data-id")
